cf3cf36b20d35014aa793efeb2c78b4313674955
de deus <dedeus.dev@gmail.com>
2023-09-30 09:09:09 -0300

    primeira rota

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-import express from 'express'
+const express = require('express')
+const app =  express();
+
+app.get("/",(req, res)=>{
+    res.send("<h1> Minha lista de Tarefas</h1>")
+})
+
+app.listen(3000,()=>{
+    console.log("Servidor iniciado!")
+})
