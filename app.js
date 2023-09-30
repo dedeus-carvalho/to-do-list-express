93acca6399707d36f0ef763e8783ac89ad570da5
de deus <dedeus.dev@gmail.com>
2023-09-30 13:54:56 -0300

    add rota post para testes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app =  express();
 app.use(express.json())
 
 
-app.use("/checklist", rotaChecklist)
+app.use("/checklist",rotaChecklist)
 
 app.listen(3000,()=>{
     console.log("Servidor iniciado!")
--- src/rotas/checklist.js
+++ src/rotas/checklist.js
@@ -4,7 +4,12 @@ const rotas = express.Router();
 
 rotas.get("/",(req, res)=>{
     console.log('estou no checklist')
-    res.send();
-})
+    res.send()
+});
+
+rotas.post("/",(req,res)=>{
+    console.log(req.body)
+    res.status(200).send(req.body)
+});
 
 module.exports = rotas;
