0dd4ecf4ad7aade2ddcbafee4a978174c81a7b78
de deus <dedeus.dev@gmail.com>
2023-10-06 14:51:05 -0300

    corrigido arquivo index.ejs

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
 
 
-const rotaChecklist = require('./src/rotas/checklist')
-const rootRouter = require('./src/rotas/index')
-require("./config/database")
+const rotaChecklist = require('./src/rotas/checklist');
+const rootRouter = require('./src/rotas/index');
+require("./config/database");
 
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
-app.set('views', path.join(__dirname, 'src/views'))
-app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'src/views'));
+app.set('view engine', 'ejs');
 
-app.use("/", rootRouter)
-app.use("/checklist", rotaChecklist)
+app.use("/", rootRouter);
+app.use("/checklist", rotaChecklist);
 
 app.listen(3000, () => {
     console.log("Servidor iniciado!")
-})
+});
