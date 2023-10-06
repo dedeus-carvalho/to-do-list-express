623402cfe64764a470c763f186fbcfea0a2d4d07
de deus <dedeus.dev@gmail.com>
2023-10-06 14:16:33 -0300

    add rota para vierws

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express')
+const path = require('path')
+
+
 const rotaChecklist = require('./src/rotas/checklist')
+const rootRouter = require('./src/rotas/index')
+require("./config/database")
+
+
 const app = express();
 app.use(express.json())
-require("./config/database")
-//require("./config/mongo")
 
+app.set('views', path.join(__dirname, 'src/views'))
+app.set('view engine', 'ejs')
 
+app.use("/", rootRouter)
 app.use("/checklist", rotaChecklist)
 
 app.listen(3000, () => {
