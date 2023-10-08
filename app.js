62a606e79a751f8064a852261d38b4cd1e1c1ded
de deus <dedeus.dev@gmail.com>
2023-10-07 21:02:28 -0300

    addicionado formulario e rotas para editar a lista de tarefas

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const methodoverrid = require('method-override');
 
 
 const rotaChecklist = require('./src/rotas/checklist');
@@ -9,6 +10,8 @@ require("./config/database");
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(methodoverrid('_method'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
--- src/rotas/checklist.js
+++ src/rotas/checklist.js
@@ -21,25 +21,45 @@ rotas.get("/:id", async(req, res)=>{
     }
 });
 
-rotas.post("/", async(req,res)=>{
-    let {nome} = req.body;
-
+rotas.get('/new', async (req, res)=>{
     try {
-      let checklist = await Checklist.create({nome})
-      res.status(200).json(checklist)
+        let checklist = new Checklist()
+        res.status(200).render('checklists/new',{checklist:checklist})
     } catch (error) {
-        res.status(422).json(error)
+        res.status(500).render('pages/error', {errors: 'Erro ao carregar o formulario'})
+    }
+})
+
+rotas.get('/:id/edit', async (req, res)=>{
+    try {
+        let checklist = await Checklist.findById(req.params.id);
+        res.status(200).render('checklists/edit', {checklist:checklist})
+    } catch (error) {
+        res.status(500).render('pages/error', {errors: 'Erro ao carregar a edição de lista de Tarefas'})
+    }
+})
+
+rotas.post("/", async(req,res)=>{
+    let {nome} = req.body.checklist;
+    let checklist = new Checklist({nome})
+    try {
+      await checklist.save()  
+      res.redirect('checklist')
+    } catch (error) {
+        res.status(422).render('checklists/new',{checklist:{...checklist, error}})
     }
     
 });
 
 rotas.put("/:id", async(req, res)=>{
-    let {nome} = req.body
+    let {nome} = req.body.Checklist
+    let checklist = await Checklist.findById(req.params.id)
     try {
-        let checklist = await Checklist.findByIdAndUpdate(req.params.id, {nome}, {new: true});
-        res.status(200).json(checklist)
+        await checklist.Update({nome});
+        res.redirect('/checklists')
     } catch (error) {
-        res.status(422).json(error)   
+        let errors = error.errors;
+        res.status(422).render('checklist/edit', {checklist:{...checklist, errors}}) 
     }
 })
 
